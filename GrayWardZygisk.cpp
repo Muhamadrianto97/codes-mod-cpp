c41610321182602071e2ebec636099443b3b5e78
Muhamad Rianto <muhamadrianto97@gmail.com>
2023-10-21 12:31:58 +0700

    Update GrayWardZygisk.cpp

--- GrayWardZygisk.cpp
+++ GrayWardZygisk.cpp
@@ -1,9 +1,30 @@
 #define GamePackageName "com.ekkorr.cf" // define the game package name here please
 
-bool noCost,addGold,addEnergy;
+bool noCost,addGold,addEnergy,win;
+int teamDamage = 1;
 
 long (*old_getUpdateEnergy)(void *instance,long energy);
 long (*old_getUpdateGold)(void *instance,long gold);
+void (*old_getOnUpdate)(void *instance,float time);
+float (*old_getTeamDamage)(void *instance);
+void (*allKillVictory)(void *instance,float time);
+
+float getTeamDamage(void *instance){
+    if (instance != NULL ){
+        float damage = old_getTeamDamage(instance);
+        float allDamage = damage *= teamDamage;
+        return allDamage;
+    }
+    return old_getTeamDamage(instance);
+}
+void getOnUpdate(void *instance,float time){
+    if(instance != NULL){
+        if(win){
+            return allKillVictory(instance,time);
+        }
+    }
+    return old_getOnUpdate(instance,time);
+}
 
 long getUpdateGold(void *instance,long gold) {
     if(instance != NULL){
@@ -14,7 +35,7 @@ long getUpdateGold(void *instance,long gold) {
         }
         if(addGold){
             gold = 50000;
-            LOGW("Berhasil Tambah Gold 50K");
+            LOGW("Success Add Gold 50K");
             addGold = false;
         }
     }
@@ -29,26 +50,37 @@ long getUpdateEnergy(void *instance,long energy) {
         }
         if(addEnergy){
             energy = 50000;
-            LOGW("Berhasil Tambah Gold 50K");
+            LOGW("Success Add Energy 50K");
             addEnergy = false;
         }
     }
     return old_getUpdateEnergy(instance,energy);
 }
+void Pointers() {
+    // allKillVictory Assembly-Csharp->Ekkorr.CF.Battle->Battle -> AllKillVictory
+    allKillVictory = (void(*)(void*, float)) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0xD509E0")));
+}
 void Hooks() {
-    // update Gold Assembly-Csharp->Ekkorr.CF.Battle->BattlePlayer -> UpdateGold
-    // update Gold Assembly-Csharp->Ekkorr.CF.Battle->BattlePlayer -> UpdateEnergy
+    // Update Gold Assembly-Csharp->Ekkorr.CF.Battle->BattlePlayer -> UpdateGold
+    // Update Energy Assembly-Csharp->Ekkorr.CF.Battle->BattlePlayer -> UpdateEnergy
+    // OnUpdate Assembly-Csharp->Ekkorr.CF.Battle->Battle -> onUpdate
+    // Update Energy Assembly-Csharp->Ekkorr.CF.Battle->EnemyPlayer -> UpdateEnergy
+
+    HOOK("0xD6B944", getTeamDamage, old_getTeamDamage);
+    HOOK("0xD50E44", getOnUpdate, old_getOnUpdate);
     HOOK("0xD68AE0", getUpdateEnergy, old_getUpdateEnergy);
     HOOK("0xD68AAC", getUpdateGold, old_getUpdateGold);
 }
 
+TextUnformatted(OBFUSCATE("Damage Tower Team And U:"));
+SliderInt(OBFUSCATE(""), &teamDamage, 1, 100, OBFUSCATE("%d"), 0);
 Checkbox(OBFUSCATE("Free Upgrade in Game"), &noCost);
-                if (Button(OBFUSCATE("Add Gold"))) {
-                    // code for button action
-                    addGold = true;
-                }
-                if (Button(OBFUSCATE("Add Energy"))) {
-                    // code for button action
-                    addEnergy = true;
-                }
+Checkbox(OBFUSCATE("Auto Win"), &win);
+TextUnformatted(OBFUSCATE("for add gold and energy u need upgrade something"));
+if (Button(OBFUSCATE("Add Gold 50K"))) {
+    addGold = true;
+}
+if (Button(OBFUSCATE("Add Energy 50K"))) {
+    addEnergy = true;
+}
 
